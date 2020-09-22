fc57622792badfd5f75d785a98d35dcad1718ce4
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 20:12:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -21,15 +21,15 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => {
-        "" => {
-        :number => , 
-        :shoe => , 
-        :points => , 
-        :rebounds => , 
-        :assists => , 
-        :steals => , 
-        :blocks => , 
-        :slam_dunks => 
+        "Jeff Adrien" => {
+        :number => 4, 
+        :shoe => 18, 
+        :points => 10, 
+        :rebounds => 1, 
+        :assists => 1, 
+        :steals => 2, 
+        :blocks => 7, 
+        :slam_dunks => 2 
       }
     }
   }
